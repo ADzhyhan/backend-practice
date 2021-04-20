6ea7203e23823efe7715266aa2f9612ea7f61604
ADzhyhan <a.dzhyhan@gmail.com>
2021-04-20 22:36:58 +0300

    add controllers in class. Change users.router and users.controller

--- src/users/users.controller.js
+++ src/users/users.controller.js
@@ -9,86 +9,85 @@ const { UserDB } = require('./models/UserDB');
 
 dotenv.config();
 
-async function profile(ctx) {
-  // const { userId } = ctx.request.params;
-  // const userResponse = await db.query(`SELECT * FROM "user" WHERE id = ${userId}`);
-  // if (!userResponse.rowCount) {
-  //   ctx.throw(400, 'User doesn`t exist');
-  // }
-  // const name = userResponse.rows[0].fname;
-
-  // await ctx.render('index', { name });
-  ctx.body = {
-    success: true,
-  };
-}
-
-async function createUser(ctx) {
-  const { body } = ctx.request;
-
-  await validator.userSchema.validateAsync(body);
-
-  body.password = crypto.pbkdf2Sync(body.password, 'salt', 100000, 64, 'sha256').toString('hex');
-
-  const createUserResponse = await db.query(`INSERT INTO  "user" (fname, lname, active, password, email) VALUES ('${body.fname}', '${body.lname}', ${body.active}, '${body.password}', '${body.email}') RETURNING *`);
-
-  const user = { ...createUserResponse.rows[0] };
-
-  ctx.status = 201;
-  ctx.body = {
-    id: user.id,
-    fname: user.fname,
-    lname: user.lname,
-    email: user.email,
-  };
-}
-
-async function logIn(ctx) {
-  await passport.authenticate('local', (err, user) => {
-    if (user) {
-      ctx.body = user;
-    } else {
-      ctx.status = 400;
-      if (err) {
-        ctx.body = { error: err };
-      }
-    }
-  })(ctx);
-}
-
-async function refresh(ctx) {
-  const token = ctx.headers.authorization.split(' ')[1];
-  const decodedToken = jwt.decode(token, process.env.refreshTokenKey);
-
-  if (decodedToken.expiresIn <= new Date().getTime()) {
-    const error = new Error('Refresh token expired, please sign in into your account.');
-    error.status = 400;
-
-    throw error;
+class UsersController {
+  static async profile(ctx) {
+    // const { userId } = ctx.request.params;
+    // const userResponse = await db.query(`SELECT * FROM "user" WHERE id = ${userId}`);
+    // if (!userResponse.rowCount) {
+    //   ctx.throw(400, 'User doesn`t exist');
+    // }
+    // const name = userResponse.rows[0].fname;
+  
+    // await ctx.render('index', { name });
+    ctx.body = {
+      success: true,
+    };
   }
-
-  const user = await UserDB.getUserByEmail(decodedToken.email);
-
-  const accessToken = {
-    id: user.id,
-    expiresIn: new Date().setTime(new Date().getTime() + 200000),
-  };
-  const refreshToken = {
-    email: user.email,
-    expiresIn: new Date().setTime(new Date().getTime() + 1000000),
-  };
-
-  ctx.body = {
-    accessToken: jwt.encode(accessToken, process.env.secretKey),
-    accessTokenExpirationDate: accessToken.expiresIn,
-    refreshToken: jwt.encode(refreshToken, process.env.refreshTokenKey),
-    refreshTokenExpirationDate: refreshToken.expiresIn,
-  };
+  
+  static async createUser(ctx) {
+    const { body } = ctx.request;
+  
+    await validator.userSchema.validateAsync(body);
+  
+    body.password = crypto.pbkdf2Sync(body.password, 'salt', 100000, 64, 'sha256').toString('hex');
+  
+    const createUserResponse = await db.query(`INSERT INTO  "user" (fname, lname, active, password, email) VALUES ('${body.fname}', '${body.lname}', ${body.active}, '${body.password}', '${body.email}') RETURNING *`);
+  
+    const user = { ...createUserResponse.rows[0] };
+  
+    ctx.status = 201;
+    ctx.body = {
+      id: user.id,
+      fname: user.fname,
+      lname: user.lname,
+      email: user.email,
+    };
+  }
+  
+  static async logIn(ctx) {
+    await passport.authenticate('local', (err, user) => {
+      if (user) {
+        ctx.body = user;
+      } else {
+        ctx.status = 400;
+        if (err) {
+          ctx.body = { error: err };
+        }
+      }
+    })(ctx);
+  }
+  
+  static async refresh(ctx) {
+    const token = ctx.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.decode(token, process.env.refreshTokenKey);
+  
+    if (decodedToken.expiresIn <= new Date().getTime()) {
+      const error = new Error('Refresh token expired, please sign in into your account.');
+      error.status = 400;
+  
+      throw error;
+    }
+  
+    const user = await UserDB.getUserByEmail(decodedToken.email);
+  
+    const accessToken = {
+      id: user.id,
+      expiresIn: new Date().setTime(new Date().getTime() + 200000),
+    };
+    const refreshToken = {
+      email: user.email,
+      expiresIn: new Date().setTime(new Date().getTime() + 1000000),
+    };
+  
+    ctx.body = {
+      accessToken: jwt.encode(accessToken, process.env.secretKey),
+      accessTokenExpirationDate: accessToken.expiresIn,
+      refreshToken: jwt.encode(refreshToken, process.env.refreshTokenKey),
+      refreshTokenExpirationDate: refreshToken.expiresIn,
+    };
+  }
 }
 
 module.exports = {
-  createUser,
-  profile,
-  logIn,
-  refresh,
+  UsersController,
 };
--- src/users/users.router.js
+++ src/users/users.router.js
@@ -1,15 +1,15 @@
 const passport = require('koa-passport');
 const Router = require('koa-router');
 
-const controllers = require('./users.controller');
+const { UsersController } = require('./users.controller');
 
 const router = new Router();
 
 // router.get('user/:userId', controllers.getUser);
-router.get('/profile', passport.authenticate('jwt', { session: false }), controllers.profile);
-router.get('/refresh/token', controllers.refresh);
-router.post('/user', controllers.createUser);
-router.post('/login', controllers.logIn);
+router.get('/profile', passport.authenticate('jwt', { session: false }), UsersController.profile);
+router.get('/refresh/token', UsersController.refresh);
+router.post('/user', UsersController.createUser);
+router.post('/login', UsersController.logIn);
 
 module.exports = {
   router,
